492b33f4e043d1ca22f60fea4860b2e1abafe9b6
Alpha17 <afonsopedrao2008@gmail.com>
2024-11-06 11:34:51 +0000

    06/11/24

--- M02/login_v2.py
+++ M02/login_v2.py
@@ -1,8 +1,8 @@
 email=str("Trump")
 password=str("ginger_rocoon")
 tentativas=1
-while tentativas<3:
- email_u=input("insira  o seu email")
+for i in range(3):
+ email_u=input("insira o seu email")
  password_u=input("insira a palvra passe") 
  if password_u==password and email_u==email:
   print("sessão iniciada com sucesso")
@@ -12,7 +12,7 @@ while tentativas<3:
  elif email_u==email and password_u!=password:
   print("O sua password está errada")
  else:
-  print(" asua password está erradaa")
+  print(" asua password está errada")
 else:
   print("esgotou as tentativas")
 
