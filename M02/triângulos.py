d60ffe89229fb623122fecb714e24e5b1ea93fb8
Alpha17 <afonsopedrao2008@gmail.com>
2024-11-08 23:36:14 +0000

    08/11/24

---
+++ M02/triângulos.py
@@ -0,0 +1,15 @@
+a=float(input("insira a medida do lado"))
+b=float(input("insira a medida do lado"))
+c=float(input("insira a medida do lado"))
+if a<0 and b<0 and c<0:
+ print("os valores têm de ser positivos")
+if (a+b)<c and (b+c)<a and (a+c)<b:
+ print("não é triângulo")
+ if a==b and b==c:
+  print("triângulo equilátero")
+ elif a==b and b!=c:
+  print("triângulo equilátero")
+  print("triângulo")
+else:
+   print("triângulo")
+   
